0e588dd72e04b56d1d5871b454b88c2d9e5951b0
CJ785 <33233362+CJ785@users.noreply.github.com>
2018-08-04 22:22:21 -0400

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers ("burger_name") VALUES ("Chicken", false);
+INSERT INTO burgers ("burger_name") VALUES ("Beef", false );
+INSERT INTO burgers ("burger_name") VALUES ("Western", false);
